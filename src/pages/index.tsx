f7574bf81d64269fc2a001e40a955d87ad69f99a
Pedro Costa <phmc99@outlook.com>
2022-11-16 18:46:17 -0300

    Next.js by @phmc99

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next App by @phmc99</title>
+        <meta name="description" content="Next App by @phmc99" />
+      </Head>
+    </div>
+  );
+}
